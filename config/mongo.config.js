8a9695f608c1c5482afd908d5cfabf20342fa2c0
Masis <masiskar@gmail.com>
2020-12-21 15:06:13 +0400

    chnaged the mongo config

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   "connection": {
-    "host": process.env.MONGO_HOST,
-    "port": process.env.MONGO_PORT,
-    "db": process.env.MONGO_DB,
-    "user": process.env.MONGO_DB_USER,
-    "pass": process.env.MONGO_DB_PASS,
-    // fixme
-    //TODO make get_mongo_url function to implement auth logic
-    "url": "mongodb://" + process.env.MONGO_HOST + ":" + process.env.MONGO_PORT + "/" + process.env.MONGO_DB
+    "url": process.env.MONGO_API
   },
-  "get_tasks_limit": 20,
 };
