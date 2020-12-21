2ad6987b09d1f88d444191630a7981e834cbe034
Masis <masiskar@gmail.com>
2020-12-21 16:27:34 +0400

    added client host

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -51,7 +51,7 @@ app.use((err, req, res, next) => {
 // Add headers
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_HOST || '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
